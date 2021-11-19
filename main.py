56c69b421dd178051bec4a5c47372864cbedf588
Eamonn Jawhary <eamonnjawhary@gmail.com>
2021-11-19 18:24:18 +0400

    test commit

--- main.py
+++ main.py
@@ -9,10 +9,26 @@ def welcome():
         """***  Unix Time Converter  ***"""
     )
 
+def menu():
+    print("""Options
+        1. Enter a timestamp
+        2. Current timestamp""")
+
 
 if __name__ == '__main__':
     print("")
     welcome()
     print("")
-    date = StampToDate.StampToDate(math.floor(time()))
-    date.calculateDate()
+    menu()
+    selection = input("Selection: ")
+
+    match selection:
+        case "1":
+            timestamp = math.floor(input("TimeStamp: "))
+            date = StampToDate.StampToDate(timestamp)
+            date.calculateDate()
+        case "2":
+            timestamp = input("TimeStamp: ")
+            date = StampToDate.StampToDate(math.floor(time()))
+            date.calculateDate()
+
